582c9adc6adce6dd8da69267761fa6e0a9cc46bd
Frendlyindy <frendlyindian@gmail.com>
2023-03-30 11:34:36 -0600

    enhancement 7

--- routes/account-route.js
+++ routes/account-route.js
@@ -4,7 +4,7 @@ const router = new express.Router();
 const regValidate = require('../utilities/account-validation')
 const utilities = require("../utilities")
 const account = require("../controllers/accountController")
-router.get("/login", account.buildLogin,
+router.get("/login", utilities.handleErrors(account.buildLogin),
 (req, res) => {
   res.status(200).send('login process')
 });
